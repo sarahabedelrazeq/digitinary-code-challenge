7acee8c84e49e3d1be502147560f0168e270ad5d
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-06 14:43:08 +0300

    website main layout

--- src/components/Layouts/Main.tsx
+++ src/components/Layouts/Main.tsx
@@ -1,6 +1,11 @@
+import React from "react";
+import { Grid } from "@mui/material";
 import Footer from "../Footer";
 import Header from "../Header";
 import styles from "./style.module.css";
+import Sidebar from "components/Sidebar";
+import RightBar from "components/RightBar";
+import PhoneNav from "components/PhoneNav";
 
 interface Props {
   children?: React.ReactNode;
@@ -8,13 +13,45 @@ interface Props {
 }
 
 const Main = (props: Props) => {
+  const [value, setValue] = React.useState(1);
+
   return (
     <div id="main-layout">
       <Header />
       <div className={styles.pageContainer} id={props.id ? props.id : "page"}>
-        {props.children}
+        <Grid
+          container
+          spacing={2}
+          justifyContent="space-between"
+        >
+          <Grid
+            item
+            lg={2}
+            xs={12}
+            sx={{ display: { xs: value !== 0 ? "none" : "", lg: "block" } }}
+          >
+            <Sidebar />
+          </Grid>
+          <Grid
+            item
+            lg={6}
+            xs={12}
+            sx={{ display: { xs: value !== 1 ? "none" : "", lg: "block" } }}
+          >
+            {props.children}
+          </Grid>
+          <Grid
+            item
+            lg={4}
+            xs={12}
+            sx={{ display: { xs: value !== 2 ? "none" : "", lg: "block" } }}
+          >
+            <RightBar />
+          </Grid>
+        </Grid>
+        <PhoneNav value={value} setValue={setValue} />
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };
