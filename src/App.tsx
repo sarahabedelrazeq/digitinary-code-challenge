9f91f50521816b846927857c68d1cfae2787f248
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-06 21:18:03 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
+import React from "react";
+import { Fallback, Auth } from "components";
+import { Route, Routes } from "react-router-dom";
+import routes from "routes";
+import withContainer from "Container";
+import "assets/sass/index.scss";
+import { ThemeProvider } from "@mui/material";
+import { mainTheme } from "themes";
 
-function App() {
+function App({ loading }: { loading: boolean }) {
   return (
     <div className="App">
+      <ThemeProvider theme={mainTheme}>
+        {loading ? (
+          <div style={{ height: "100vh" }}>
+            <Fallback />
+          </div>
+        ) : (
+          <React.Suspense
+            fallback={
+              <div style={{ height: "100vh" }}>
+                <Fallback />
+              </div>
+            }
+          >
+            <Routes>
+              {routes.map((route, index) => {
+                return (
+                  route.component && (
+                    <Route
+                      key={index}
+                      element={
+                        <Auth {...route}>
+                          <route.component />
+                        </Auth>
+                      }
+                      {...route}
+                    />
+                  )
+                );
+              })}
+            </Routes>
+          </React.Suspense>
+        )}
+      </ThemeProvider>
     </div>
   );
 }
 
-export default App;
+export default withContainer(React.memo(App));
