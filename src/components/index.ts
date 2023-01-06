0e02ce329db9e529b3c8c3b378a8144c118ea85e
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-06 21:17:04 +0300

    update components index

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Fallback from "./Fallback";
+import ErrorBoundary from "./ErrorBoundary";
+import Auth from "./Auth";
+import Sidebar from "./Sidebar";
+import RightBar from "./RightBar";
+import PhoneNav from "./PhoneNav";
+
+export { Footer, Header, Fallback, ErrorBoundary, Auth, Sidebar, RightBar, PhoneNav };
