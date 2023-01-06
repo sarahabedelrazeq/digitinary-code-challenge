5435a6baf22ac8b2b7fa62df1adabcb0d8b3f7c9
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-06 14:42:33 +0300

    website side bar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,71 @@
+import {
+  AccountBox,
+  Home,
+  ModeNight,
+  Person,
+  Language,
+} from "@mui/icons-material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+
+const Sidebar = () => {
+  return (
+    <Box>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary="Home Page" sx={{ color: "text.primary" }} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountBox />
+            </ListItemIcon>
+            <ListItemText primary="Profile" sx={{ color: "text.primary" }} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ModeNight />
+            </ListItemIcon>
+            <Switch onChange={(e) => {}} />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Language />
+            </ListItemIcon>
+            <ListItemText primary="Change Language" onClick={(e) => {}} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
+
+export default Sidebar;
